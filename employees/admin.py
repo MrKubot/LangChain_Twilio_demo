61690e9f0f3af565870524fee6a58ba92a5012c4
MrKubot <mateusz.wasielewski2001@gmail.com>
2025-01-30 01:51:40 +0100

    Database schema

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Department, Employee
 
 # Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'role', 'department', 'phone_number',)
+    list_filter = ('role', 'department',)
+    search_fields = ('first_name', 'last_name', 'role',)
+
+    def full_name(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
+    full_name.short_description = 'Pracownik'
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'employee_count',)
+    search_fields = ('name',)
+
+    def employee_count(self, obj):
+        return obj.employees.count()
+    employee_count.short_description = 'Liczba pracownikow'
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Role(models.TextChoices):
+    MAGAZYNIER = ('MGZN', 'Magazynier',)
+    BRYGADZISTA = ('BRGD', 'Brygadzista',)
+    MANAGER = ('MNGR', 'Manager',)
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    role = models.CharField(max_length=4, choices=Role.choices)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+    supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.get_role_display()}: {self.first_name} {self.last_name} | {self.phone_number}'
+
